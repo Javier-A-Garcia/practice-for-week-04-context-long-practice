62dede7e3ca8398d65a4b70213ace4df8aa74ebf
Javier Garcia <javiergarcia7991@gmail.com>
2022-07-03 22:21:48 -0700

    Completed practice

--- problems/09-call-on-target.js
+++ problems/09-call-on-target.js
@@ -1,7 +1,7 @@
 function callOnTarget(func, obj1, obj2) {
   // Your code here
+  return func.bind(obj1, obj2)();
 }
-
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
